6f7701b95de399d0623ad33999b019335b0f8d62
QingXia-Ela <Elahere@163.com>
2022-11-03 18:06:36 +0800

    Update pages

--- src/declare/THREE/addons.ts
+++
@@ -1,7 +0,0 @@
-export abstract class addonsBasic {
-  public Geometry?: THREE.Object3D
-  /** 渲染更新函数 */
-  public update?: Function
-  /** 模型改变触发函数 */
-  public ChangeModel?: (...args: any) => void
-}
