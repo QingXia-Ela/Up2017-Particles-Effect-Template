9ee64ddb98be56d5ee0d5b33f92c2cd17e1a3abe
QingXia-Ela <Elahere@163.com>
2022-11-04 20:17:36 +0800

    chore: 去除多余代码

--- src/pages/IndexPage/index.tsx
+++ src/pages/IndexPage/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import * as THREE from 'three'
 import Styles from './index.module.scss'
 import ParticleSystem from '@/THREE'
 import { useEffect, useRef } from 'react'
@@ -7,7 +6,6 @@ import AtmosphereParticle from '@/THREE/atmosphere'
 import { ParticleModelProps } from '@/declare/THREE'
 import Tween from '@tweenjs/tween.js'
 import GetFlatGeometry from '@/utils/GetFlatGeometry'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 function IndexPage() {
   const wrapper = useRef<HTMLDivElement | null>(null)
@@ -71,12 +69,6 @@ function IndexPage() {
     {
       name: 'ball',
       path: new URL('../../THREE/models/examples/ball.obj', import.meta.url).href,
-      loader: {
-        loaderInstance: new GLTFLoader(),
-        load(args) {
-          return new THREE.BufferGeometry()
-        }
-      },
       onLoadComplete(Geometry) {
         Geometry.scale(scaleNum, scaleNum, scaleNum)
         Geometry.translate(-600, 0, -100)
